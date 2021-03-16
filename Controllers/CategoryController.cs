fd2ec56df2dc5619ff9247027f3f2a6eec4f2ebd
Rico-Jose <imbangjoserico@gmail.com>
2021-03-16 13:16:20 +0800

    add the category to the database

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -23,9 +23,20 @@ namespace Rocky.Controllers
             return View(objList);
         }
 
+        // GET - Create
         public IActionResult Create()
         {
             return View();
+        }
+
+        // POST - Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+            {
+            _db.Category.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index"); 
         }
     }
 }
